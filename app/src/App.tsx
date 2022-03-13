cfd8960cabafb374e86d6dde9a4ba7142aa794d1
Kim Hyunjae <loma1016@gmail.com>
2022-03-13 13:45:05 +0900

    컴파일 에러
    컴파일 에러가 생기는데 아직 해결하지 못함

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Card from './components/Card/Card';
 import Chart from './components/Chart/Chart';
+import BasicTable from './components/BasicTable/BasicTable';
 
 function App() {
   return (
     <div>
       <Card title={'test'} count={20} subText={'subText'} subTextNum={55} />
-      <Chart width={800} />
+      {/* <Chart width={800} /> */}
+      <BasicTable />
     </div>
   );
 }
